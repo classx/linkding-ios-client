8d7d95bf7f530539acecfd27d30312f3da3a9f39
Alexey Nikandrov <alexey@Alexeys-Mac-mini.local>
2023-06-18 17:05:29 +0300

    init project

---
+++ linkding/linkdingApp.swift
@@ -0,0 +1,17 @@
+//
+//  linkdingApp.swift
+//  linkding
+//
+//  Created by Alexey Nikandrov on 18/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct linkdingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
