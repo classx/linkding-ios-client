73244946e394c3b0ee9905646c21477ce47b62dc
Alexey Nikandrov <alexey@Alexeys-Mac-mini.local>
2023-06-23 17:51:58 +0300

    Fix NetManager
    добавлено - параметры headers

--- linkding/Data/NetworkManager.swift
+++ linkding/Data/NetworkManager.swift
@@ -12,6 +12,7 @@ import Combine
 /// It's responsible for getting any kind of data from an URL.
 ///
 class NetworkManager: NetworkLayer {
+    let headers = ["Authorization": "Token 91327d880c0fb9619831b8cd282aa5843e8a4419"]
 
     private enum DataError: Error {
         case cantDecode
@@ -20,7 +21,13 @@ class NetworkManager: NetworkLayer {
 
     /// Gets data from an URL, retries three times in case of network errors.
     func getData(url: URL) -> AnyPublisher<Data, Error> {
-        return URLSession.shared.dataTaskPublisher(for: url)
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+
+        for (key, value) in headers {
+            request.setValue(value, forHTTPHeaderField: key)
+        }
+        return URLSession.shared.dataTaskPublisher(for: request)
             .tryMap {
                 try self.checkResponse(dataTaskOutput: $0, url: url)
             }
@@ -30,11 +37,9 @@ class NetworkManager: NetworkLayer {
 
     ////  Returns the data from the network call if there are no errors in the response.
     private func checkResponse(dataTaskOutput: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
-
-        guard let response = dataTaskOutput.response as? HTTPURLResponse, response.statusCode >= 200 && response.statusCode < 300 else {
-            throw DataError.badResponse(response: dataTaskOutput.response, url: url)
-        }
-
-        return dataTaskOutput.data
+            guard let response = dataTaskOutput.response as? HTTPURLResponse, response.statusCode >= 200 && response.statusCode < 300 else {
+                throw DataError.badResponse(response: dataTaskOutput.response, url: url)
+            }
+            return dataTaskOutput.data
         }
 }
