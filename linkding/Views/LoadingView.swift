f5a105f2b8677bb2a1c7bcb3773d5cd42ba2c9d8
Alexey Nikandrov <alexey@Alexeys-Mac-mini.local>
2023-06-26 19:53:48 +0300

    add Loaded view
    need add Activity view

---
+++ linkding/Views/LoadingView.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingView.swift
+//  linkding
+//
+//  Created by Alexey Nikandrov on 26/06/2023.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text("Loading")
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
