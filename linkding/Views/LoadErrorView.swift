614ba14db92ec8b56fa8defb1dc93e05b65cfeac
Alexey Nikandrov <alexey@Alexeys-Mac-mini.local>
2023-06-26 19:54:08 +0300

    add error view
    on API load

---
+++ linkding/Views/LoadErrorView.swift
@@ -0,0 +1,20 @@
+//
+//  LoadErrorView.swift
+//  linkding
+//
+//  Created by Alexey Nikandrov on 26/06/2023.
+//
+
+import SwiftUI
+
+struct LoadErrorView: View {
+    var body: some View {
+        Text("Eroooo")
+    }
+}
+
+struct LoadErrorView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadErrorView()
+    }
+}
