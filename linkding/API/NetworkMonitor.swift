2c7fc561c0f0dbce0c07b85c9f6a2709b1d33a0f
Alexey Nikandrov <alexey@Alexeys-Mac-mini.local>
2023-07-15 20:47:24 +0300

    add network monitor

---
+++ linkding/API/NetworkMonitor.swift
@@ -0,0 +1,27 @@
+//
+//  NetworkMonitor.swift
+//  linkding
+//
+//  Created by Alexey Nikandrov on 15/07/2023.
+//
+
+import Foundation
+import Network
+
+class NetworkMonitor: ObservableObject {
+    private let networkMonitor = NWPathMonitor()
+    private let workerQueue = DispatchQueue(label: "Monitor")
+    var isConnected = false
+
+    init() {
+        networkMonitor.pathUpdateHandler = { path in
+            self.isConnected = path.status == .satisfied
+            Task {
+                await MainActor.run {
+                    self.objectWillChange.send()
+                }
+            }
+        }
+        networkMonitor.start(queue: workerQueue)
+    }
+}
